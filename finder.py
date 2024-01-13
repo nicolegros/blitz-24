c6e949516df9ebf163b349d171e26025f73fccb8
nicolegros <nicolaslegros98@gmail.com>
2024-01-13 09:34:31 -0500

    :sparkles: Kill enemies with the lesser health

--- finder.py
+++ finder.py
@@ -5,10 +5,15 @@ class Finder:
     def find_enemy_position(self, gamemessage: GameMessage) -> Vector:
         enemy_ships = self.find_enemy_ships(gamemessage)
         if len(enemy_ships) > 0:
-            return enemy_ships[0].worldPosition
+            alive_ships = list(filter(lambda ship: ship.currentHealth > 0, enemy_ships))
+            ship_with_minimal_health = alive_ships.sort(key=lambda ship: ship.currentHealth)[0]
+            return ship_with_minimal_health
         else:
             return Vector(0, 0)
 
     def find_enemy_ships(self, gamemessage: GameMessage) -> list[Ship]:
         team_id = gamemessage.currentTeamId
-        return list(filter(lambda ship: ship.teamId in [shipId for shipId in gamemessage.shipsPositions.keys() if shipId != team_id], gamemessage.ships.values()))
+        ships = list(filter(lambda ship: ship.teamId in [shipId for shipId in gamemessage.shipsPositions.keys() if
+                                                     shipId != team_id], gamemessage.ships.values()))
+        print(f"SHIPS: {list(map( lambda ship: {'id': ship.teamId, 'currentHealth': ship.currentHealth, 'position': ship.worldPosition}, ships))}")
+        return ships
